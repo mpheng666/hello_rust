a3fb513fd01edcda5066c0769913deb0ff4c1e32
mpheng666 <mpheng96@gmail.com>
2022-05-28 10:15:25 +0800

    add guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,32 +1,61 @@
 // rust automatically brings in 'prelude' standard lib
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    // use 'let' statement to create variable
-    // variables are immutable by default in rust
-    let apples = 5; //immutable
-    // add 'mut' to make it mutable
-    let mut guess = String::new(); //mutable
+    println!("The secret number is: {}", secret_number);
 
-    // 'String::new()' returns a new string instance, empty string
-    // 'new' function is an associated function of the 'String' type
-    // 'new' comes with manu types
+    loop    {
 
-    // we can also use 'std::io::stdin()' instead
-    io::stdin()
-        // '&' reference is immutable by default
-        .read_line(&mut guess)
-        // try to create new line when a method is called for readability
-        // rust will warn if '.expect' is not used
-        .expect("Failed to read line");
+        println!("Please input your guess.");
 
-        // 'readline' return io::Result, type of enum
-        // returns 'Ok' if success, 'Err' if failed
+        // use 'let' statement to create variable
+        // variables are immutable by default in rust
+        // let apples = 5; //immutable
+        // add 'mut' to make it mutable
+        let mut guess = String::new(); //mutable
 
-    // {} is used as a placeholder
-    println!("You guess: {}", guess);
+        // 'String::new()' returns a new string instance, empty string
+        // 'new' function is an associated function of the 'String' type
+        // 'new' comes with manu types
+
+        // we can also use 'std::io::stdin()' instead
+        io::stdin()
+            // '&' reference is immutable by default
+            .read_line(&mut guess)
+            // try to create new line when a method is called for readability
+            // rust will warn if '.expect' is not used
+            .expect("Failed to read line");
+
+            // 'readline' return io::Result, type of enum
+            // returns 'Ok' if success, 'Err' if failed
+
+        let guess: u32 = match guess.trim().parse()   {
+            Ok(num) => num,
+            // '_' is a catchall value
+            Err(_) => {
+                println!("Please key in a valid number");
+                continue;
+                }
+        };
+
+        // {} is used as a placeholder
+        println!("You guess: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+                }
+        }
+    }
 }
+    
+    // cargo doc --open is useful
